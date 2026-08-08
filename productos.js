104fa9ff7c78880bbbc88f05a1907b5d82babe72
Saavedra-catalogo <jsaavedraolivos1025@gmail.com>
2026-08-08 15:17:07 -0400

    Actualizacion automatica del catalogo 08-08-2026 15:17:07,23

--- productos.js
+++ productos.js
@@ -162,6 +162,10 @@ window.PRODUCT_PREVIEW_LINKS = {
 };
 window.PRODUCTOS_SAAVEDRA = [
   {
+    "nombre": "Elevador de Riel Picatinny",
+    "slug": "elevador-de-riel-picatinny"
+  },
+  {
     "nombre": "PCP QL-1000LW 5.5MM",
     "slug": "pcp-ql-1000lw-5-5mm"
   },
