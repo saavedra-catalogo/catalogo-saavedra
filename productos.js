1b0032284b2bcf04ef285b0cf8a1f16d145c26ee
Saavedra-catalogo <jsaavedraolivos1025@gmail.com>
2026-08-15 16:07:04 -0400

    Actualizacion automatica del catalogo 15-08-2026 16:07:04,86

--- productos.js
+++ productos.js
@@ -530,6 +530,10 @@ window.PRODUCTOS_SAAVEDRA = [
     "slug": "abrazadera-ajustable-con-riel"
   },
   {
+    "nombre": "Soporte Riel Picatinny a 45°- Montaje Lateral",
+    "slug": "soporte-riel-picatinny-a-45-montaje-lateral"
+  },
+  {
     "nombre": "Kit de Limpieza para Rifles .22 / 5.5 mm",
     "slug": "kit-de-limpieza-para-rifles-22-5-5-mm"
   },
